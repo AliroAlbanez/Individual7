e4eaee1a50bccb2d97859e2d21c06a7e89f44b13
Aliro Albanez <134630959+Chikenface@users.noreply.github.com>
2023-05-31 09:42:39 -0400

    Update MainActivity.java
    versión final

--- app/src/main/java/com/example/individual7/MainActivity.java
+++ app/src/main/java/com/example/individual7/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.individual7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //variables para el mensaje toast a utilizar
+        Context context =getApplicationContext();
+        CharSequence text="Bienvenido/a a la Aplicación";
+        int duracion = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duracion);
+        toast.show()
     }
 }
